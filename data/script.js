97da40e372b9cdccdb2c620f30e32e15dff9e3b8
K3;3 <41787377+Wisien999@users.noreply.github.com>
2022-03-06 01:06:43 +0100

    Refactor

--- data/script.js
+++ data/script.js
@@ -1,9 +1,11 @@
 const API_URL = "/"
 const GET_POWER_STATE_URL = `${API_URL}/power`
 const SET_POWER_STATE_URL = `${API_URL}/power`
+const CHANGE_MODE_URL = `${API_URL}/mode`
 
 function init() {
-
+    // document.getElementById("js-disabled-buttons").style.display = "none";
+    document.getElementById("js-enabled-buttons").style.display = "";
 }
 
 // setInterval(function () { getSensorData(); }, 1000); // Call the update function every set interval e.g. 1000mS or 1-sec
@@ -11,8 +13,9 @@ function init() {
 function getPowerState() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("power-status").innerHTML = this.responseText;
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("power-status").innerText = "The power is " + this.responseText;
+            document.getElementById("power-button-js-enabled").innerText = "Turn " + (this.responseText === "ON" ? "OFF" : "ON")
         }
     };
     xhttp.open("GET", GET_POWER_STATE_URL, true);
@@ -21,5 +24,14 @@ function getPowerState() {
 
 function setPowerState() {
     let xhttp = new XMLHttpRequest();
-    xhttp.open("POST", )
+    xhttp.onreadystatechange = function () {
+        if (this.readyState !== 4) {
+            return;
+        }
+
+        getPowerState();
+    }
+
+    xhttp.open("PATCH", SET_POWER_STATE_URL, true)
+    xhttp.send();
 }
